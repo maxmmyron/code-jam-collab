5cda04f78e8f18a5b313260df57e6f6854b63d2a
BuildTools <unconfigured@null.spigotmc.org>
2023-09-19 20:40:14 -0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CodeJam",
+  description: "Making finding projectmates easier",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
