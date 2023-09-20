c9010bdf1531c83f401ccdbca488c28fdb8048d8
BuildTools <unconfigured@null.spigotmc.org>
2023-09-19 21:31:51 -0400

    connected prisma to pscale

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <h1 className="text-xl m-4">Hello Worldington</h1>
       <h2 className="text-lg m-4"><a href="https://www.youtube.com/watch?v=H-3YrkLZU44" className="text-blue-500 underline">Bozo Dubbed Over</a></h2>
+      <h3>Bruhhhhhhhhhhhhhhhhhington!</h3>
     </>
   );
 }
