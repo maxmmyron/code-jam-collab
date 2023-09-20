49f95fec743b6dd764bb36d7401ed5774c7b857b
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-09-19 21:14:19 -0400

    bozo dubbed over

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-xl m-4">Hello Worldington</h1>
+    <>
+      <h1 className="text-xl m-4">Hello Worldington</h1>
+      <h2 className="text-lg m-4"><a href="https://www.youtube.com/watch?v=H-3YrkLZU44" className="text-blue-500 underline">Bozo Dubbed Over</a></h2>
+    </>
   );
 }
