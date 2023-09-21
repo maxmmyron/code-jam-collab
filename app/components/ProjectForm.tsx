42489603f2046231526c6551d95f93e4e3e5cd50
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-09-21 08:46:36 -0400

    add project POST endpoint

--- app/components/ProjectForm.tsx
+++ app/components/ProjectForm.tsx
@@ -1,10 +1,41 @@
 'use client'
 
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 
 const ProjectForm = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleProjectSubmission = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+
+    const formData = new FormData(event.currentTarget);
+
+    const res = await fetch('/api/v1/projects', {
+      body: formData,
+      method: 'POST',
+    })
+
+    const data = await res.json();
+    setLoading(false);
+  };
+
   return (
-    <form></form>
+    <form onSubmit={handleProjectSubmission}>
+      <label>
+        Project Name
+        <input type="text" name="name" />
+      </label>
+
+      <label>
+        Project Description
+        <textarea name="description" />
+      </label>
+
+      <button type="submit">Create Project</button>
+
+      <p>{loading ? 'Loading...' : ''}</p>
+    </form>
   )
 }
 
