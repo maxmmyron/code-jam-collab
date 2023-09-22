3b8188727fb48f3b78c4723e43bea3ff40fd7a8d
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-09-22 10:08:16 -0400

    add session error checking to API endpoint

--- app/api/v1/projects/route.ts
+++ app/api/v1/projects/route.ts
@@ -7,11 +7,16 @@ export async function POST(request: Request) {
   const session = await getServerSession(authOptions)
   const body = await request.json()
   const { name, description } = body.formData
+
+  if(!session) {
+    return NextResponse.redirect('/api/auth/signin');
+  }
+
   await prisma.project.create({
     data: {
       name: name,
       description: description,
-      owner: { connect: { email: session?.user?.email }}
+      owner: { connect: { email: session?.user?.email as string }}
     }
   });
   return NextResponse.json({hello: 'world'})
