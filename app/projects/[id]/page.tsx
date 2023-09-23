cd18a60d024e3410ad0bd44dc4f371b21154edfe
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-09-23 16:25:23 -0400

    that suspense was useless

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,5 +1,4 @@
 import Prisma from "@prisma/client";
-import { Suspense } from "react";
 import prisma from "../../../lib/prisma";
 import Project from "../../components/Project";
 
@@ -15,14 +14,10 @@ async function getProject(id: string): Promise<Prisma.Project | null> {
 const Page = async ({ params }: { params: { id: string } }) => {
   const project = await getProject(params.id);
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      {project === null ? (
-        <div>Project not found</div>
-      ) : (
-        <Project project={project} />
-      )}
-    </Suspense>
+  return project === null ? (
+    <div>Project not found</div>
+  ) : (
+    <Project project={project} />
   );
 };
 
