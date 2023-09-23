a6a6fd1ec1fad0f225f87aa76d9f22c4a0135f00
BuildTools <unconfigured@null.spigotmc.org>
2023-09-23 11:42:14 -0400

    fixed prod fetch and router refresh on submit form

--- app/components/ProjectForm.tsx
+++ app/components/ProjectForm.tsx
@@ -2,18 +2,21 @@
 
 import { FormEvent, useState } from "react";
 import { Session } from "next-auth";
+import { useRouter } from "next/navigation";
 
 const ProjectForm = (session: Session) => {
+  const router = useRouter();
   const handleProjectSubmission = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
 
     try {
-      const res = await fetch("http://localhost:3000/api/v1/projects", {
+      const res = await fetch("/api/v1/projects", {
         method: "POST",
         body: JSON.stringify(Object.fromEntries(formData)),
       });
+      router.refresh();
     } catch (e) {
       console.error(e);
     }
