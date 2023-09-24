6d6657275ae1286666a9bf779d2f0eddacc9fc14
BuildTools <unconfigured@null.spigotmc.org>
2023-09-23 20:29:25 -0400

    projects can now be deleted baby

---
+++ app/components/DeleteButton.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+type DeleteProps = {
+  id: string;
+  projectName: string;
+};
+
+const DeleteButton = (props: DeleteProps) => {
+  const { id, projectName } = props;
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    const verification = confirm(
+      `Are you sure you want to delete Project: ${projectName}`,
+    );
+    if (!verification) return;
+    try {
+      const response = await fetch(`/api/v1/projects/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      //TODO Find less cringe way to do this
+      router.push("/projects");
+      router.refresh();
+      if (!response.ok)
+        throw new Error(`${response.status}: (${response.statusText})`);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div
+      className="border bg-black px-3 py-2 rounded-md w-fit text-white"
+      onClick={handleDelete}
+    >
+      Delete
+    </div>
+  );
+};
+
+export default DeleteButton;
--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,15 +1,18 @@
 import Prisma from "@prisma/client";
 import prisma from "../../../lib/prisma";
 import Project from "../../components/Project";
+import DeleteButton from "../../components/DeleteButton";
 
-async function getProject(id: string): Promise<Prisma.Project & {owner: Prisma.User} | null> {
+async function getProject(
+  id: string,
+): Promise<(Prisma.Project & { owner: Prisma.User }) | null> {
   const project = await prisma.project.findUnique({
     where: {
       id,
     },
     include: {
       owner: true,
-    }
+    },
   });
   return project;
 }
@@ -17,17 +20,23 @@ async function getProject(id: string): Promise<Prisma.Project & {owner: Prisma.U
 const Page = async ({ params }: { params: { id: string } }) => {
   const project = await getProject(params.id);
 
-  if(!project) throw new Error("Project not found");
+  if (!project) throw new Error("Project not found");
 
   const owner = project.owner;
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    console.log("yo");
+  };
 
   return project === null ? (
     <div>Project not found</div>
   ) : (
-    <>
+    <div className="mx-8">
       <Project project={project} />
       <p>by {owner.name}</p>
-    </>
+      <DeleteButton id={params.id} projectName={project.name} />
+    </div>
   );
 };
 
