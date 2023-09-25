c8f294725b5081c6cdd995bb428f339806e99c08
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-09-24 20:49:30 -0400

    update route to include PUT endpoint for project slugs

--- app/api/v1/projects/[id]/route.ts
+++ app/api/v1/projects/[id]/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../../lib/prisma";
-import { getServerSession } from "next-auth";
-import authOptions from "../../../auth/[...nextauth]/options";
 
 export async function DELETE(request: Request, { params }) {
   const { id } = params
@@ -9,6 +7,26 @@ export async function DELETE(request: Request, { params }) {
     await prisma.project.delete({
       where: {
         id: id
+      }
+    })
+
+    return NextResponse.json({ id: id }, { status: 201 })
+  } catch(e) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
+}
+
+export async function PUT(request: Request, { params }) {
+  const { id } = params
+  const { name, description } = await request.json()
+  try {
+    await prisma.project.update({
+      where: {
+        id: id
+      },
+      data: {
+        name: name,
+        description: description
       }
     })
 
