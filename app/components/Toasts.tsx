f52130fc9415b9f5f9b6ef9dcd9dd1d20dc3a22b
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-09-29 16:42:23 -0400

    update toast design, add manual close button

--- app/components/Toasts.tsx
+++ app/components/Toasts.tsx
@@ -1,21 +1,28 @@
 "use client";
 
 import { toastStore } from "../utils/zustand";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Toasts = () => {
   const store = toastStore();
 
   return (
     <>
-      <div className="fixed bottom-0 right-0 p-4">
-        {store.toasts.map((toast) => (
-          <div
-            key={Number(toast.timeoutID)}
-            className="bg-black text-white rounded-md p-4"
-          >
-            {toast.message}
-          </div>
-        ))}
+      <div className="fixed bottom-0 right-0 p-4 max-w-md space-y-4">
+        <AnimatePresence>
+          {store.toasts.map((toast) => (
+            <motion.article
+              key={Number(toast.timeoutID)}
+              className="flex flex-nowrap gap-4 rounded-xl p-4 border shadow bg-white"
+              initial={{ opacity: 0, x: 300 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 300 }}
+            >
+              <p className="text-neutral-700">{toast.message}</p>
+              <button className="w-6 h-6 flex justify-center items-center p-2 rounded bg-neutral-700 text-white" onClick={() => toast.remove()}>✖</button>
+            </motion.article>
+          ))}
+        </AnimatePresence>
       </div>
     </>
   );
